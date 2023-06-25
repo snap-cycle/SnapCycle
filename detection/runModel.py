18bcea5716ee5a3bcae9c6d0e8e2d5bd21fc772e
Luai Bashar <luai.hawa@gmail.com>
2023-06-25 18:43:11 -0400

    Added summary of objects detected per run

--- detection/runModel.py
+++ detection/runModel.py
@@ -24,7 +24,19 @@ def runModel():
 
     # Use the model and set custom configurations
     sourcePath = './images/capture.jpg'
-    model.predict(task = 'detect', source = sourcePath, save = True)
+    results = model.predict(task = 'detect', source = sourcePath, save = True)
+
+    #Print each object detected and their info
+    result = results[0]
+    for box in result.boxes:
+        class_id = result.names[box.cls[0].item()]
+        cords = box.xyxy[0].tolist()
+        cords = [round(x) for x in cords]
+        conf = round(box.conf[0].item(), 2)
+        print("Object type:", class_id)
+        print("Coordinates:", cords)
+        print("Probability:", conf)
+        print("---")
     
 #uncomment this when you want to run this file, otherwise it must stay commented for imports to work
-#runModel()
+# runModel()
