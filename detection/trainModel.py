ddc85779049be354c9b634b098f4f14e84c541d4
Luai Bashar <luai.hawa@gmail.com>
2023-06-27 22:33:37 -0400

    Added some functions for training

--- detection/trainModel.py
+++ detection/trainModel.py
@@ -1,1 +1,20 @@
 from ultralytics import YOLO
+
+# Loads and trains model. Trains on Single CPU and GPU
+def trainNewModel():
+
+    model = YOLO('yolov8n.yaml')
+    model.train(data='coco128.yaml', epochs=100, imgsz=640)
+
+def trainPreTrainedModel():
+    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+    model.train(data='coco128.yaml', epochs=100, imgsz=640)
+
+def resumeTraining():
+    model = YOLO('path/to/last.pt')  # load a partially trained model
+    model.train(resume=True)
+
+# Uncomment function based on what is wanted to be used
+# trainNewModel()
+# trainPreTrainedModel()
+# resumeTraining()
