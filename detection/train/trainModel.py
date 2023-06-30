7ae1638063768cfbfdea0e0287d7e0dcfebc4fa3
Luai Bashar <luai.hawa@gmail.com>
2023-06-29 23:48:04 -0400

    added clearml configuration

--- detection/train/trainModel.py
+++ detection/train/trainModel.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+from clearml import Task
 
 # Read description.md to understand training
 def trainNewModel():
@@ -8,6 +9,9 @@ def trainNewModel():
 
 
 def trainPreTrainedModel():
+    # Call ClearML init before YOLOv8
+    task = Task.init(project_name="YOLOv8", task_name="detection_training", tags=['YOLOv8'])
+
     model = YOLO('yolov8n.pt')  # load a pretrained model, can either be yolo's or someone elses
     model.train(data='instructions.yaml', epochs=100, imgsz=640,
                 batch=16, patience=50)
@@ -18,3 +22,5 @@ def resumeTraining():
     model.train(resume=True)
 
 # trainPreTrainedModel()
+
+# Set up clearML, and start setting up dataset
