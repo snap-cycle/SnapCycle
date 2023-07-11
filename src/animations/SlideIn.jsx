afb0f14d671b94538247bec55ec8decc06b733ac
Aidan Froggatt <aidanfr@live.ca>
2023-07-11 16:04:23 -0400

    change slide to rely on viewport width rather than pixel measurement

--- src/animations/SlideIn.jsx
+++ src/animations/SlideIn.jsx
@@ -11,7 +11,7 @@ const SlideIn = ({ children, invert }) => {
             <span
                 style={{
                     // default is to slide in from left to RIGHT
-                    transform: isInView ? "none" : "translateX(" + (invert === true ? 1920 : -1920) +"px)",
+                    transform: isInView ? "none" : "translateX(" + (invert === true ? 100 : -100) +"vw)",
                     opacity: isInView ? 1 : 0,
                     transition: "all 1.0s cubic-bezier(0.17, 0.55, 0.55, 1)"
                 }}
