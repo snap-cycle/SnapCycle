017d804bfc31dbe928c1138bd32d42eb6c00dd06
Aidan Froggatt <aidanfr@live.ca>
2023-07-11 16:59:42 -0400

    slightly less shitty page animation

--- src/animations/AnimatedPage.jsx
+++ src/animations/AnimatedPage.jsx
@@ -1,9 +1,15 @@
 import { motion } from "framer-motion";
 
 const animations = {
-    initial: { opacity: 0, x: 100 },
-    animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: -100 },
+    initial: { y: -20, opacity: 0 },
+    animate: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.7,
+            ease: [0.6, -0.05, 0.01, 0.99],
+        },
+    },
 };
 
 const AnimatedPage = ({ children }) => {
