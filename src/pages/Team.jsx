85679e189a2c8ac6caef022572012b682dd24572
Aidan Froggatt <aidanfr@live.ca>
2023-07-11 14:27:49 -0400

    removed old content from team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -7,12 +7,7 @@ const Team = () => {
     return (
         <div>
             <h2>Our Team</h2>
-            <p>Aidan, <s>Harry</s>, Luai, Gabe</p>
-            <h2>Our Mission</h2>
-            <p>Within the last year only 8% of total discarded plastics were recycled!</p>
-            <p>We hope to tackle the recycling issue from the ground up, revolutionizing recycling as we know it</p>
-            <h2>Contact Us</h2>
-            <p>aidanfr@live.ca</p>
+            <p>Aidan, Luai, Gabe?, <s>Harry</s></p>
         </div>
     );
 };
