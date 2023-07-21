d872c0f59a138fa5902757c09dc1b73fc29f400a
Luai Bashar <luai.hawa@gmail.com>
2023-07-20 23:42:20 -0400

    Refined NewFooter

--- src/components/NewFooter.jsx
+++ src/components/NewFooter.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import '../styles/NewFooter.css';
+import WhiteLogo from "../assets/branding/WhiteLogo.png";
+import OutlineButton from "./OutlineButton";
 
 const NewFooter = ({pages}) => {
 
 
     return (
-        <div className='newFooter'>
-            <div className='footerLeft'>
-                <div>
-                    Hello
+        <div>
+            <div className='MainFooter'>
+                <div className='FooterLeft'>
+                    <div className='FooterLogoName'>
+                        <img src={WhiteLogo} alt="SnapCycle Logo" className="FooterLogoImage"/>
+                        <div className='SnapcycleTitle'>
+                            SnapCycle
+                        </div>
+                    </div>
+                    <div className='ProjectSlogan'>
+                        Saving the turtles one snap at a time.
+                    </div>
+                    <div className='TryNowMessage'>
+                        Try now at now cost!
+                    </div>
+                    <OutlineButton title="Demo" destination="/demo"/>
+                    <hr className='Divider'/>
+                    <div className='SocialMedia'>
+
+                    </div>
                 </div>
-                <div>
-                    Hi
-                </div>
+            </div>
+            <div className='BottomFooter'>
+                
             </div>
         </div>
     )
