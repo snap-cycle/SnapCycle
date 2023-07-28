bb7564c422f23122007b073d9d5443866d2954e6
Luai Bashar <luai.hawa@gmail.com>
2023-07-28 14:10:57 -0400

    Added start for navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BlackLogo from "../assets/branding/BlackLogo.png";
 import SnapCycleTitle from "../assets/NavBar/SnapCycleTitle.png";
 import OutlineButton from "./OutlineButton";
@@ -6,43 +6,66 @@ import HamburgerMenu from "../assets/NavBar/HamburgerMenu.jpg";
 import "../styles/NavBar.css";
 import {useLocation, useNavigate} from "react-router-dom";
 
-const Navbar = ({pages}) => {
+const Navbar = ({ pages }) => {
+  const [showPartialBackground, setShowPartialBackground] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+  const [scrollCount, setScrollCount] = useState(0);
+  const scrollThreshold = 200;
 
-    return (
-        <div className='NavBar'>
-            <div className='NavBarLogoAndName'>
-                <img src={BlackLogo} alt="SnapCycle Logo" className="NavBarLogo"/>
-                <img src={SnapCycleTitle} alt="SnapCycle Title" className='NavBarTitle' />
-            </div>
-            <div className='NavBarPages'>
-                {
-                    pages.map((page) =>
-                        <NavbarPageItem page={page}/>
-                    )
-                }
-            </div>
-            <div className='ButtonContainer'>
-                <OutlineButton title="Try Now" destination="/demo" className="NavBarButton"/>
-                <img src={HamburgerMenu} alt="Hamburger Menu" className='HamburgerMenu'/>
-            </div>
-        </div>
-    );
-}
+  // Add an event listener to detect scroll events
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollCount((scrollCount) => scrollCount + 1);
+    };
 
-const NavbarPageItem = ({page}) => {
-    const destination = "/" + page.toLowerCase();
-    const location = useLocation();
-    const navigate = useNavigate();
-    const goToLocation = (location) => {
-        navigate(location);
-    }
+    window.addEventListener('scroll', handleScroll);
 
-    return (
-        <div className={location.pathname === destination ? "NavBarPagesActive" : "NavBarPagesInactive"}
-             onClick={() => goToLocation(destination)}>
-            {page}
-        </div>
-    );
-}
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Update the background when the scroll count reaches the threshold
+  useEffect(() => {
+    if (scrollCount >= scrollThreshold) setShowBackground(true);
+  }, [scrollCount]);
+
+  return (
+    <div className="NavBar" id={showBackground ? "NavBarSolid" : "NavBarTransparent" }>
+      <div className='NavBarLogoAndName'>
+        <img src={BlackLogo} alt="SnapCycle Logo" className="NavBarLogo" />
+        <img src={SnapCycleTitle} alt="SnapCycle Title" className='NavBarTitle' />
+      </div>
+      <div className='NavBarPages'>
+        {pages.map((page) => (
+          <NavbarPageItem page={page} key={page} />
+        ))}
+      </div>
+      <div className='ButtonContainer'>
+        <OutlineButton title="Try Now" destination="/demo" className="NavBarButton" />
+        <img src={HamburgerMenu} alt="Hamburger Menu" className='HamburgerMenu' />
+      </div>
+    </div>
+  );
+};
+
+const NavbarPageItem = ({ page }) => {
+  const destination = "/" + page.toLowerCase();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const goToLocation = (location) => {
+    navigate(location);
+  };
+
+  return (
+    <div
+      className={location.pathname === destination ? "NavBarPagesActive" : "NavBarPagesInactive"}
+      onClick={() => goToLocation(destination)}
+    >
+      {page}
+    </div>
+  );
+};
 
 export default Navbar;
