3b2014114aab1f2bf26edbeddaf323d37171c73f
Aidan Froggatt <aidanfr@live.ca>
2023-08-04 16:49:34 -0400

    created file for team info

---
+++ src/Info/TeamInfo.jsx
@@ -0,0 +1,26 @@
+export const TeamInfo = {
+    teamMembers: [
+        {
+            name: "Aidan Froggatt",
+            links: {
+                linkedin: "https://www.linkedin.com/in/aidanfroggatt/",
+                github: "https://github.com/aidanfroggatt",
+                portfolio: "https://aidanfroggatt.github.io/portfolio/"
+            },
+            role: "",
+            imageSrc: "",
+            imageAlt: "Aidan's Profile Picture"
+        },
+        {
+            name: "Luai Bashar",
+            links: {
+                linkedin: "https://www.linkedin.com/in/luaibashar/",
+                github: "https://github.com/luaibash",
+                portfolio: ""
+            },
+            role: "",
+            imageSrc: "",
+            imageAlt: "Luai's Profile Picture"
+        }
+    ]
+}
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import '../styles/Team.css';
 import AnimatedPage from "../animations/AnimatedPage";
+import {TeamInfo} from "../Info/TeamInfo";
 
 const Team = () => {
     useEffect(() => {
@@ -9,9 +10,25 @@ const Team = () => {
 
     return (
         <AnimatedPage>
-            <div className="TeamPanelLanding">
+            <div className="team-landing-panel">
                 <h2>Our Team</h2>
                 <p>Aidan, Luai, Gabe, Harry</p>
+            </div>
+            <div className="team-members-container">
+                {
+                    TeamInfo.teamMembers.map((person, index) => (
+                        <div className="team-member" key={index}>
+                            <div className="team-member-name">{person.name}</div>
+                            <div className="team-member-role">{person.role}</div>
+                            <div className="team-member-image">
+                                <img src={person.imageSrc} alt={person.imageAlt}/>
+                            </div>
+                            <div className="team-member-links-container">
+
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </AnimatedPage>
     );
