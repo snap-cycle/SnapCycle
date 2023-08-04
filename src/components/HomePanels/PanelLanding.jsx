e05508069ee2c1407bdc1eb136fc65c754764064
Aidan Froggatt <aidanfr@live.ca>
2023-08-04 15:36:29 -0400

    added overlay to landing page

--- src/components/HomePanels/PanelLanding.jsx
+++ src/components/HomePanels/PanelLanding.jsx
@@ -1,11 +1,16 @@
 import '../../styles/Home/PanelLanding.css';
 import React from "react";
+import BackgroundImage from "../../assets/Home/RecyclingBackground.jpg";
 
 const PanelLanding = () => {
 
     return (
         <div className='HomePanelLanding'>
-
+            <div className="panel-home-title">
+                <img className="landing-background-image" alt="Background Image For Landing Page" src={BackgroundImage}/>
+                <div className="overlay"></div>
+                <div className="homepage-title">SnapCycle</div>
+            </div>
         </div>
     )
 }
