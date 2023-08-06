11429fa21cec16dcb54586169346c129c12c31cf
Luai Bashar <luai.hawa@gmail.com>
2023-08-06 13:01:08 -0400

    Added statistic row change

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,8 +1,35 @@
+import React, {useState, useEffect} from "react";
 import CountUp from "react-countup";
-import React from "react";
 import '../styles/components/Statistics.css';
 
 const Statistics = ({ statistics }) => {
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const semiRowBreakpoint = 1150;
+    const columnBreakpoint = 750;
+    useEffect(() => {
+        const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // StatisticsRow is used for one row of statistics or one column. This will switch accordingly
+    if (windowWidth > semiRowBreakpoint || windowWidth <= columnBreakpoint) {
+        return <StatisticsRow statistics={statistics}/>
+    }
+    // StatisticsSemiRow is used for two rows of two statistics.
+    else {
+        return <StatisticsSemiRow statistics={statistics}/>
+    }
+}
+
+const StatisticsRow = ({ statistics }) => {
 
     return (
         <div className='StatisticsContainer'>
@@ -18,6 +45,15 @@ const Statistics = ({ statistics }) => {
                     </div>
                 ))
             }
+        </div>
+    )
+}
+
+const StatisticsSemiRow = ({ statistics}) => {
+
+    return (
+        <div>
+
         </div>
     )
 }
