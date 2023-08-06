80204c1e132d89e0715248107825632d93168e98
Luai Bashar <luai.hawa@gmail.com>
2023-08-06 17:47:26 -0400

    Made panel four responsive

--- src/Info/StatisticsInfo.jsx
+++ src/Info/StatisticsInfo.jsx
@@ -1,6 +1,6 @@
 export const StatisticsInfo = [
     {name: "StatisticOne", value: "12700000", suffix: "", subtext: "tonnes of plastic are littered into the ocean every year."},
     {name: "StatisticTwo", value: "11", suffix: " Masks", subtext: "are littered every hour."},
-    {name: "StatisticThree", value: "60", suffix: " %", subtext: "of water pollution is attributed\nto litter."},
-    {name: "StatisticFour", value: "100000", suffix: "", subtext: "animals are killed by plastic bags every year.\n"},
+    {name: "StatisticThree", value: "60", suffix: " %", subtext: "of water pollution is attributed to litter."},
+    {name: "StatisticFour", value: "100000", suffix: "", subtext: "animals are killed by plastic bags every year."},
 ]
