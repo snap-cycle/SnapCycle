964d14ff8cf9bdee3cfd5f34db79497900fdde6b
Aidan Froggatt <aidanfr@live.ca>
2023-08-10 16:40:23 -0400

    context for app theme added

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,33 @@
+import {createContext, useEffect, useState} from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    const [darkMode, setDarkMode] = useState(prefersDarkMode);
+
+    useEffect(() => {
+        // Listen to changes in user's system preference
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const handleChange = (event) => {
+            setDarkMode(event.matches);
+        };
+
+        mediaQuery.addListener(handleChange);
+
+        // Clean up listener on component unmount
+        return () => {
+            mediaQuery.removeListener(handleChange);
+        };
+    }, []);
+    const toggleTheme = () => {
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
