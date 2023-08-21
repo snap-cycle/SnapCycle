a53ba5517fea2e5fed5a260a4153d392f1d1cbdf
Aidan Froggatt <aidanfr@live.ca>
2023-08-21 16:36:56 -0400

    refactoring

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -52,7 +52,8 @@ const Team = () => {
                                 </div>
                             </div>
                             )
-                    })}
+                        })
+                    }
                 </div>
             </div>
         </AnimatedPage>
