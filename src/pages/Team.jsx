a9f3bfa88169cb16d50c5c48d213cfd8e5aa8a1e
Aidan Froggatt <aidanfr@live.ca>
2023-08-21 14:53:27 -0400

    created very basic cards for team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,41 +6,43 @@ import {TeamInfo} from "../Info/TeamInfo";
 const Team = () => {
     return (
         <AnimatedPage>
-            <div className="team-landing-panel">
-                <h2>Our Team</h2>
-            </div>
-            <div className="team-members-container">
-                {
-                    Object.keys(TeamInfo.teamMembers).map((key) => {
-                        return (
-                        <div className="team-member">
-                            <img className="team-member-image" src={TeamInfo.teamMembers[key].imageSrc} alt={TeamInfo.teamMembers[key].imageAlt}/>
-                            <div className="team-member-name">{TeamInfo.teamMembers[key].name}</div>
-                            <div className="team-member-role">{TeamInfo.teamMembers[key].role}</div>
-                            <div className="team-member-bio">{TeamInfo.teamMembers[key].bio}</div>
-                            <div className="team-member-contact-container">
-                                <div className="team-member-link-container">
-                                    {
-                                        Object.keys(TeamInfo.teamMembers[key].links).map((link) => {
-                                            return (
-                                                <div>{TeamInfo.teamMembers[key].links[link]}</div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <div className="team-member-info-container">
-                                    {
-                                        Object.keys(TeamInfo.teamMembers[key].personaInfo).map((info) => {
-                                            return (
-                                                <div>{TeamInfo.teamMembers[key].personaInfo[info]}</div>
-                                            )
-                                        })
-                                    }
+            <div className="team">
+                <div className="team-landing-panel">
+                    <h2>Our Team</h2>
+                </div>
+                <div className="team-members-container">
+                    {
+                        Object.keys(TeamInfo.teamMembers).map((key) => {
+                            return (
+                            <div className="team-member">
+                                {/*<img className="team-member-image" src={TeamInfo.teamMembers[key].imageSrc} alt={TeamInfo.teamMembers[key].imageAlt}/>*/}
+                                <div className="team-member-name">{TeamInfo.teamMembers[key].name}</div>
+                                <div className="team-member-role">{TeamInfo.teamMembers[key].role}</div>
+                                <div className="team-member-bio">{TeamInfo.teamMembers[key].bio}</div>
+                                <div className="team-member-contact-container">
+                                    <div className="team-member-link-container">
+                                        {
+                                            Object.keys(TeamInfo.teamMembers[key].links).map((link) => {
+                                                return (
+                                                    <div>{TeamInfo.teamMembers[key].links[link]}</div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <div className="team-member-info-container">
+                                        {
+                                            Object.keys(TeamInfo.teamMembers[key].personaInfo).map((info) => {
+                                                return (
+                                                    <div>{TeamInfo.teamMembers[key].personaInfo[info]}</div>
+                                                )
+                                            })
+                                        }
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        )
-                })}
+                            )
+                    })}
+                </div>
             </div>
         </AnimatedPage>
     );
