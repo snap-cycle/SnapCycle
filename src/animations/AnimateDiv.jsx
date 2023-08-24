4d438ff62a08cb2fc357890ca414b1b1ce4922b4
Luai Bashar <luai.hawa@gmail.com>
2023-08-23 21:44:49 -0400

    Added delay parameter to animation component

--- src/animations/AnimateDiv.jsx
+++ src/animations/AnimateDiv.jsx
@@ -7,14 +7,14 @@ import '../styles/Home/PanelThree.css';
 import '../styles/Home/PanelFour.css';
 import '../styles/Home/PanelFive.css';
 
-const AnimateDiv = ({ children, down = false}) => {
+const AnimateDiv = ({ children, delay = 0, down = false}) => {
 
     return (
         <motion.div 
             initial={{ opacity: 0, y: (down) ? -50 : 50 }} 
             whileInView={{ opacity: 1, y: 0}}
             viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.1, ease: "easeOut" }}
+            transition={{ duration: 0.5, delay: 0.1 + delay, ease: "easeOut" }}
         >
             {children}
         </motion.div>
