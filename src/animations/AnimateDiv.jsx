14fee6d1515e67cea7ccaefa6afe3668e86390e1
Luai Bashar <luai.hawa@gmail.com>
2023-08-24 14:58:31 -0400

    Added children delay component

--- src/animations/AnimateDiv.jsx
+++ src/animations/AnimateDiv.jsx
@@ -1,4 +1,5 @@
-import {motion} from "framer-motion";
+import {React, useRef, Children} from "react";
+import {motion, useInView} from "framer-motion";
 import '../styles/Home/Home.css';
 import '../styles/Home/PanelLanding.css';
 import '../styles/Home/PanelOne.css';
@@ -7,19 +8,38 @@ import '../styles/Home/PanelThree.css';
 import '../styles/Home/PanelFour.css';
 import '../styles/Home/PanelFive.css';
 
-const AnimateDiv = ({ children, className, id, delay = 0, down = false}) => {
+const AnimateDiv = ({ children, className, id, down = false, childrenDelay = false}) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once: true});
 
-    return (
+    // Animating one div
+    if (!childrenDelay) return (
         <motion.div 
             initial={{ opacity: 0, y: (down) ? -50 : 50 }} 
             whileInView={{ opacity: 1, y: 0}}
             viewport={{ once: true }}
-            transition={{ duration: 0.5, delay: 0.1 + delay, ease: "easeOut" }}
+            transition={{ duration: 0.5, delay: 0.1, ease: "easeOut" }}
         >
             <div className={`${className}`} id={`${id}`}>
                 {children}
             </div>
         </motion.div>
+    );
+
+    // Animating a div with animated children elements that each have an incrementing delay
+    else return (
+        <div className={`${className}`} id={`${id}`} ref={ref}>
+            {Children.map(children, (child, index) => (
+                <motion.div
+                    key={index}
+                    initial={{ opacity: 0, y: (down) ? -50 : 50 }}
+                    animate={ isInView ? { opacity: 1, y: 0 } : {} }
+                    transition={{ duration: 0.5, delay: 0.1 + index * 0.2, ease: "easeOut" }}
+                >
+                    {child}
+                </motion.div>
+            ))}
+        </div>
     );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,23 +75,25 @@ const PanelOne = () => {
 const PanelTwo = () => {
     return <div className='PanelTwo'>
         <div className='PanelTwoText'>
-            <div className='Title'>
-                Take a Snap of Any
-                Item, Anywhere.
-            </div>
-            <div className='SubText'>
-                Take a picture of the object in front of you,
-                that&#39;s all you have to do! Let SnapCycle do
-                the rest as it detects the object for you,
-                and lets you in on all the sweet disposing details.
-            </div>
-            <div className='SubText'>
-                Try out SnapCycle and experience the power of smooth
-                recycling today.
-            </div>
-            <div className='PanelTwoButton'>
-                <OutlineButton title="Try Now" destination="/demo"/>
-            </div>
+            <AnimateDiv childrenDelay={true}>
+                <div className='Title'>
+                    Take a Snap of Any
+                    Item, Anywhere.
+                </div>
+                <div className='SubText'>
+                    Take a picture of the object in front of you,
+                    that&#39;s all you have to do! Let SnapCycle do
+                    the rest as it detects the object for you,
+                    and lets you in on all the sweet disposing details.
+                </div>
+                <div className='SubText'>
+                    Try out SnapCycle and experience the power of smooth
+                    recycling today.
+                </div>
+                <div className='PanelTwoButton'>
+                    <OutlineButton title="Try Now" destination="/demo"/>
+                </div>
+            </AnimateDiv>
         </div>
         <div className='PanelTwoImages'>
             <img src={PanelTwoLaptopPhone} alt="Laptop and Phone" className='PanelTwoLaptopPhone'/>
@@ -104,7 +106,7 @@ const PanelThree = () => {
         <div className='PanelThreeImageContainer'>
             <img src={PanelThreePlaceholder} alt="Placeholder" className='PanelThreePlaceholder'/>
         </div>
-        <div className='PanelThreeText'>
+        <AnimateDiv className='PanelThreeText'>
             <div className='Title'>
                 Can't Take a Picture?
                 No Worries!
@@ -122,7 +124,7 @@ const PanelThree = () => {
             <div className='PanelThreeButton'>
                 <OutlineButton title="Try Now" destination="/demo"/>
             </div>
-        </div>
+        </AnimateDiv>
     </div>;
 }
 
