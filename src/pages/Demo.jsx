fd1c68fdaf8727df3b9414fa2d06614e11230683
Luai Bashar <luai.hawa@gmail.com>
2023-08-29 13:36:20 -0400

    Removed useless code

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -24,7 +24,7 @@ const DemoPanel = () => {
 const CameraPanel = () => {
 
     return (
-        <div className="CameraContainer">
+        <div className="CameraPanel">
             {/* <div className="CameraButtonContainer">
                 <button className="CameraButton">
                     Enable Camera
