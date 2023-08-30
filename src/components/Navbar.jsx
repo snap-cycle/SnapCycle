a31b4346c04ab8c76b0a42ddc1eeefe8cc52a8cd
Aidan Froggatt <aidanfr@live.ca>
2023-08-29 21:48:52 -0400

    hamburger menu now works

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,180 +14,193 @@ import "../styles/Navbar/HamburgerMenu.css";
 import * as PagesInfo from "../Info/PagesInfo";
 
 const Navbar = () => {
-  const [showHamburgerMenu, setHamburgerMenu] = useState(false);
+    const [showHamburgerMenu, setHamburgerMenu] = useState(false);
 
-  const updateHamburgerMenu = () => {
-    setHamburgerMenu(showHamburgerMenu => !showHamburgerMenu);
+    const updateHamburgerMenu = () => {
+        setHamburgerMenu(showHamburgerMenu => !showHamburgerMenu);
 
-    SwitchScrollBar();
-  };
-
-  //useEffect to close hamburger menu whenever window is large enough to not need it
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 1000) {
-        setHamburgerMenu(false);
-        document.body.style.overflow = 'auto';
-      }
+        SwitchScrollBar();
     };
 
-    window.addEventListener('resize', handleResize);
+    //useEffect to close hamburger menu whenever window is large enough to not need it
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 1000) {
+                setHamburgerMenu(false);
+                document.body.style.overflow = 'auto';
+            }
+        };
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // Switches scrollbars whenever hamburger menu is opened or closed
+    const SwitchScrollBar = () => {
+        if (showHamburgerMenu) {
+            document.body.style.overflow = 'auto';
+            document.querySelector('#HamburgerMenuActive').style.overflow = 'hidden';
+        }
+        else {
+            setTimeout(() => {
+                document.body.style.overflow = 'hidden';
+                document.querySelector('#HamburgerMenuActive').style.overflow = 'auto';
+            }, 500 );
+        }
     };
-  }, []);
 
-  // Switches scrollbars whenever hamburger menu is opened or closed
-  const SwitchScrollBar = () => {
-    if (showHamburgerMenu) {
-      document.body.style.overflow = 'auto';
-      document.querySelector('#HamburgerMenuActive').style.overflow = 'hidden';
-    }
-    else {
-      setTimeout(() => {
-        document.body.style.overflow = 'hidden';
-        document.querySelector('#HamburgerMenuActive').style.overflow = 'auto';
-      }, 500 );
-    }
-  };
-  
-  return (
-    <AnimatedPage y={0} duration={0} delay={0.1}>
-      <ActiveNavbar updateHamburgerMenu={updateHamburgerMenu}/>
-      <HamburgerMenu showHamburgerMenu={showHamburgerMenu} updateHamburgerMenu={updateHamburgerMenu}/>
-    </AnimatedPage>
-  );
+    return (
+        <AnimatedPage y={0} duration={0} delay={0.1}>
+            <ActiveNavbar updateHamburgerMenu={updateHamburgerMenu}/>
+            <HamburgerMenu showHamburgerMenu={showHamburgerMenu} updateHamburgerMenu={updateHamburgerMenu}/>
+        </AnimatedPage>
+    );
 }
 
 
 const ActiveNavbar = ({updateHamburgerMenu}) => {
-  const [showPartialBackground, setPartialBackground] = useState(false);
-  const [showSolidBackground, setSolidBackground] = useState(false);
-  const [showNavBar, setNavBar] = useState(true);
-  const [currentScroll, setCurrentScroll] = useState(window.scrollY);
-  const partialThreshold = 75, solidThreshold = 150, navBarThreshold = 550;
+    const [showPartialBackground, setPartialBackground] = useState(false);
+    const [showSolidBackground, setSolidBackground] = useState(false);
+    const [showNavBar, setNavBar] = useState(true);
+    const [currentScroll, setCurrentScroll] = useState(window.scrollY);
+    const partialThreshold = 75, solidThreshold = 150, navBarThreshold = 550;
 
-  const handleNavigation = useCallback((e) => {
-      const window = e.currentTarget;
-      //If past home panel and scrolling down, nav bar disappears. If scroll up, reappears
-      if (window.scrollY > navBarThreshold && currentScroll < window.scrollY) setNavBar(false);
-      else setNavBar(true);
-      
-      setCurrentScroll(window.scrollY);
-  }, [currentScroll]);
+    const handleNavigation = useCallback((e) => {
+        const window = e.currentTarget;
+        //If past home panel and scrolling down, nav bar disappears. If scroll up, reappears
+        if (window.scrollY > navBarThreshold && currentScroll < window.scrollY) setNavBar(false);
+        else setNavBar(true);
 
-  //useEffect to track whether user has scrolled up or down last
-  useEffect(() => {
-    setCurrentScroll(window.scrollY);
-    window.addEventListener("scroll", handleNavigation);
+        setCurrentScroll(window.scrollY);
+    }, [currentScroll]);
 
-    return () => {
-      window.removeEventListener("scroll", handleNavigation);
-    };
-  }, [handleNavigation]);
+    //useEffect to track whether user has scrolled up or down last
+    useEffect(() => {
+        setCurrentScroll(window.scrollY);
+        window.addEventListener("scroll", handleNavigation);
 
-  //useEffect to change appearance of navbar when it is at top of page
-  useEffect(() => {
-    const handleScroll = () => {
-      //If scrolled down once, show partial translucent background
-      if (window.scrollY > partialThreshold) setPartialBackground(true);
-      else setPartialBackground(false);
-      //If scrolled down two or more times, show full background
-      if (window.scrollY > solidThreshold) setSolidBackground(true);
-      else setSolidBackground(false);
-    };
+        return () => {
+            window.removeEventListener("scroll", handleNavigation);
+        };
+    }, [handleNavigation]);
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    //useEffect to change appearance of navbar when it is at top of page
+    useEffect(() => {
+        const handleScroll = () => {
+            //If scrolled down once, show partial translucent background
+            if (window.scrollY > partialThreshold) setPartialBackground(true);
+            else setPartialBackground(false);
+            //If scrolled down two or more times, show full background
+            if (window.scrollY > solidThreshold) setSolidBackground(true);
+            else setSolidBackground(false);
+        };
 
-  return (
-    <div className='NavbarContainer'>
-      <div className='ActiveNavbarContainer' id={showNavBar ? (showPartialBackground ? (showSolidBackground ? "NavbarSolid" : "NavbarPartial") : "NavbarTransparent") : "NavbarDissapear"}>
-        <div className="ActiveNavbar">
-          <div className='NavbarLogoAndName'>
-            <img src={BlackLogo} alt="SnapCycle Logo" className="NavbarLogo" />
-            <img src={BlackSnapCycleTitle} alt="SnapCycle Title" className='NavbarTitle' />
-          </div>
-          <div className='NavbarPages'>
-            {PagesInfo.PagesInfo.map((page, index) => (
-              <NavbarPageItem pageName={page.name} key={index} />
-            ))}
-          </div>
-          <div className='NavbarRightContainer'>
-            <div className='NavbarButtonContainer'>
-              <OutlineButton title="Try Now" destination="/demo"/>
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className='NavbarContainer'>
+            <div className='ActiveNavbarContainer' id={showNavBar ? (showPartialBackground ? (showSolidBackground ? "NavbarSolid" : "NavbarPartial") : "NavbarTransparent") : "NavbarDissapear"}>
+                <div className="ActiveNavbar">
+                    <div className='NavbarLogoAndName'>
+                        <img src={BlackLogo} alt="SnapCycle Logo" className="NavbarLogo" />
+                        <img src={BlackSnapCycleTitle} alt="SnapCycle Title" className='NavbarTitle' />
+                    </div>
+                    <div className='NavbarPages'>
+                        {PagesInfo.PagesInfo.map((page, index) => (
+                            <NavbarPageItem pageName={page.name} key={index} />
+                        ))}
+                    </div>
+                    <div className='NavbarRightContainer'>
+                        <div className='NavbarButtonContainer'>
+                            <OutlineButton title="Try Now" destination="/demo"/>
+                        </div>
+                        <img src={HamburgerIcon} alt="Hamburger Icon" className='HamburgerIcon' id={showPartialBackground ? 'HamburgerIconGray' : 'HamburgerIconWhite'} onClick={() => updateHamburgerMenu()}/>
+                    </div>
+                </div>
             </div>
-            <img src={HamburgerIcon} alt="Hamburger Icon" className='HamburgerIcon' id={showPartialBackground ? 'HamburgerIconGray' : 'HamburgerIconWhite'} onClick={() => updateHamburgerMenu()}/>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 const NavbarPageItem = ({ pageName }) => {
-  const destination = "/" + pageName.toLowerCase();
-  const location = useLocation();
-  const navigate = useNavigate();
-  const goToLocation = (location) => {
-    navigate(location);
-  };
+    const destination = "/" + pageName.toLowerCase();
+    const location = useLocation();
+    const navigate = useNavigate();
+    const goToLocation = (location) => {
+        navigate(location);
+    };
 
-  return (
-    <div
-      className={location.pathname === destination ? "NavbarPagesActive" : "NavbarPagesInactive"}
-      onClick={() => goToLocation(destination)}
-    >
-      {pageName}
-    </div>
-  );
+    return (
+        <div
+            className={location.pathname === destination ? "NavbarPagesActive" : "NavbarPagesInactive"}
+            onClick={() => goToLocation(destination)}
+        >
+            {pageName}
+        </div>
+    );
 };
 
 const HamburgerMenu = ({showHamburgerMenu, updateHamburgerMenu}) => {
 
-  return (
-      <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'}>
-        <div className='HamburgerMenu'>
-          <div className='BurgerTopContainer' id={ showHamburgerMenu ? 'BurgerTopContainerActive' : 'BurgerTopContainerInactive'}>
-            <div className='BurgerLogoAndName'>
-              <img src={WhiteLogo} alt="SnapCycle Logo" className="HamburgerLogo"/>
-              <div className='BurgerTitleContainer'>
-                <img src={WhiteSnapCycleTitle} alt="SnapCycle Title" className='HamburgerTitle'/>
-                <div className='cover' id={showHamburgerMenu && 'LogoActive'}></div>
-              </div>
+    return (
+        <div className='HamburgerMenuContainer' id={showHamburgerMenu ? 'HamburgerMenuActive' : 'HamburgerMenuInactive'}>
+            <div className='HamburgerMenu'>
+                <div className='BurgerTopContainer' id={ showHamburgerMenu ? 'BurgerTopContainerActive' : 'BurgerTopContainerInactive'}>
+                    <div className='BurgerLogoAndName'>
+                        <img src={WhiteLogo} alt="SnapCycle Logo" className="HamburgerLogo"/>
+                        <div className='BurgerTitleContainer'>
+                            <img src={WhiteSnapCycleTitle} alt="SnapCycle Title" className='HamburgerTitle'/>
+                            <div className='cover' id={showHamburgerMenu && 'LogoActive'}></div>
+                        </div>
+                    </div>
+                    <img src={WhiteX} alt="Close Button" className='WhiteX' onClick={() => updateHamburgerMenu()}/>
+                </div>
+                <div className='HamburgerPages'>
+                    <HamburgerPage show={showHamburgerMenu} text={'Home'} page={0} destination="/home" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                    <HamburgerPage show={showHamburgerMenu} text={'Try Now'} page={1} destination="/demo" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                    <HamburgerPage show={showHamburgerMenu} text={'Team'} page={2} destination="/team" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                    <HamburgerPage show={showHamburgerMenu} text={'FAQ'} page={3} destination="/faq" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                    <HamburgerPage show={showHamburgerMenu} text={'Help'} page={4} destination="/help" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                    <HamburgerPage show={showHamburgerMenu} text={'Contact Us'} page={5} destination="/contact" hamburgerMenu={showHamburgerMenu} setHamburgerMenu={updateHamburgerMenu}/>
+                </div>
             </div>
-            <img src={WhiteX} alt="Close Button" className='WhiteX' onClick={() => updateHamburgerMenu()}/>
-          </div>
-          <div className='HamburgerPages'>
-            <HamburgerPage show={showHamburgerMenu} text={'Home'} page={0}/>
-            <HamburgerPage show={showHamburgerMenu} text={'Try Now'} page={1}/>
-            <HamburgerPage show={showHamburgerMenu} text={'Team'} page={2}/>
-            <HamburgerPage show={showHamburgerMenu} text={'FAQ'} page={3}/>
-            <HamburgerPage show={showHamburgerMenu} text={'Help'} page={4}/>
-            <HamburgerPage show={showHamburgerMenu} text={'Contact Us'} page={5}/>
-          </div>
         </div>
-      </div>
-  );
+    );
 };
 
-const HamburgerPage = ({show, text, page}) => {
-  const maxPage = 5; const interval = 0.05; const maxTime = maxPage*interval;
-  const delay = (show) ? (0.5 + page*interval) : (maxTime - page*interval);
-  const pageStyle = {
-    transition: `padding-left 0.5s, color 0.5s, opacity 0.2s ease-out ${delay}s, margin-top 0.2s ease-out ${delay}s`,
-  };
+const HamburgerPage = ({show, text, page, destination, hamburgerMenu, setHamburgerMenu}) => {
+    const maxPage = 5; const interval = 0.05; const maxTime = maxPage*interval;
+    const delay = (show) ? (0.5 + page*interval) : (maxTime - page*interval);
+    const pageStyle = {
+        transition: `padding-left 0.5s, color 0.5s, opacity 0.2s ease-out ${delay}s, margin-top 0.2s ease-out ${delay}s`,
+    };
 
-  return (
-    <div className='HamburgerPage' id={show ? 'HamburgerPageActive' : 'HamburgerPageInactive'} style={pageStyle}>
-      {text}
-      <img src={RightArrow} alt="Arrow" className='HamburgerArrow'/>
-    </div>
-  );
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const goToLocation = (location) => {
+        navigate(location);
+        setHamburgerMenu(showHamburgerMenu => !showHamburgerMenu);
+    }
+
+    return (
+        <div
+            className='HamburgerPage'
+            id={show ? 'HamburgerPageActive' : 'HamburgerPageInactive'}
+            style={pageStyle}
+            onClick={() => goToLocation(destination)}
+        >
+            {text}
+            <img src={RightArrow} alt="Arrow" className='HamburgerArrow'/>
+        </div>
+    );
 };
 
 export default Navbar;
