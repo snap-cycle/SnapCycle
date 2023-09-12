594ab5374e4d6c401b16e243d0326b9112ebd6fe
Aidan Froggatt <aidanfr@live.ca>
2023-09-12 14:16:22 -0400

    added info file for all result page content

---
+++ src/Info/ItemInfo.jsx
@@ -0,0 +1,33 @@
+import PlasticBottleBackground from "../assets/Result/MainBackground1.svg";
+import PlasticBottle from "../assets/Result/PlasticBottle.svg";
+import RecyclingBin from "../assets/Result/RecyclingBin.svg";
+import BottleAndArrow from "../assets/Result/BottleAndArrow.svg";
+import WaterBottleCrushed from "../assets/Result/WaterBottleCrushed.svg";
+export const itemInfo = {
+    "Plastic Bottle": {
+        "name": "Plastic Bottle",
+        "background": PlasticBottleBackground,
+        "dispsal": {
+            1: {
+                "text": "Empty and rinse",
+                "image": PlasticBottle
+            },
+            2: {
+                "text": "Recycling bin",
+                "image": RecyclingBin
+            },
+            3: {
+                "text": "Keep caps on",
+                "image": BottleAndArrow
+            },
+            4: {
+                "text": "Do not crush",
+                "image": WaterBottleCrushed
+            }
+        },
+        "info": "this is plastic lol",
+        'additionalAssets': {
+
+        }
+    }
+}
