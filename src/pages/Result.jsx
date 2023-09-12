b9d673058e35fbe0d8004ed4d4c5717c900d9dbb
Aidan Froggatt <aidanfr@live.ca>
2023-09-12 14:10:52 -0400

    css file for result created

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -5,11 +5,8 @@ const Result = () => {
     switch (item) {
         case "Plastic Bottle":
             return (
-                <div>
-                    <h1>Plastic Bottle</h1>
-                    <h2>Recycle</h2>
-                    <h3>Recycle this item in your blue bin!</h3>
-                    <h4>Why?</h4>
+                <div className="">
+
                 </div>
             )
     }
