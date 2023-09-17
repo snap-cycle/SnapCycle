2315d27ce6da2b6f03a169386f4b7d33412ea1cf
Aidan Froggatt <aidanfr@live.ca>
2023-09-16 22:31:55 -0400

    switched browser router for hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Demo from './pages/Demo';
