f2f7489d392aecc2db21538239ab5051192febfa
Luai Bashar <luai.hawa@gmail.com>
2023-09-20 20:05:05 -0400

    Added foundation for member

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import { React, useRef, useEffect } from 'react';
 import '../styles/pages/Team.css';
 import AnimatedPage from "../animations/AnimatedPage";
+import Luai from '../assets/Team/Luai.png';
 
 const Team = () => {
 
     return (
         <AnimatedPage>
             <div className="TeamPanel">
-                Test
+                <div className='Title'>
+                    Meet The Team.
+                </div>
+                <div className="TeamTitleDivider"/>
+                <div className='SubText' id='TeamSubtext'>
+                    Meet the people who make SnapCycle possible!
+                </div>
+                <div className='MembersContainer'>
+                    <Member name="Luai Bashar" role="Software Developer" headshot={Luai} colour="#FF6161"/>
+                </div>
             </div>
         </AnimatedPage>
+    );
+};
+
+const Member = ({name, role, headshot, colour}) => {
+    const headshotRef = useRef(null);
+
+    useEffect(() => {
+        headshotRef.current.style.backgroundColor = colour;
+    }, [colour]);
+
+    return (
+        <div className='MemberContainer'>
+            <div className='HeadshotContainer' ref={headshotRef}>
+                <img src={headshot} alt="" className='Headshot'/>
+            </div>
+            <div className='TextContainer'>
+                <div className='MemberName'>
+                    {name}
+                </div>
+                <div className='MemberRole'>
+                    {role}
+                </div>
+            </div>
+        </div>
     );
 };
   
